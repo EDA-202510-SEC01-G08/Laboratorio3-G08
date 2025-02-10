3a7e51624aedeefcb5d4adf645423c26fe6bede4
Archilipeper <p.archila@uniandes.edu.co>
2025-02-10 16:56:00 -0500

    funciones: is_empty(), change_info(), last_element(). Single linked list

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -70,3 +70,26 @@ def remove_first(my_list):
         my_list["size"] -= 1
         return first["info"]
 
+def is_empty(my_list):
+    if my_list["size"] == 0:
+        return True
+    else: 
+        return False
+    
+def last_element(my_list):
+    last_element = my_list["last"]
+    return last_element
+
+def change_info(my_list, pos, new_info):
+    if pos < 0 or pos > my_list["size"] -1:
+        raise Exception('IndexError: list index out of range')
+    else: 
+        count = 0
+        actual = my_list["first"]
+        while actual != None and count != pos:
+            count += 1
+            actual = actual["next"]
+        actual["info"] = new_info
+        return actual
+
+
